50001907cb0c38f757c2ce675ebbf7d34e8c8caf
joshua <joshuanankai@163.com>
2021-06-17 22:41:12 +0800

    1-mac升级后重装了xcode 2-重新定位了git的位置 3-修改了pycharm不提示报错的问题 4-新建了一个工程，所有功能实验完毕

---
+++ TestOs.py
@@ -0,0 +1,15 @@
+import os
+
+
+class Test:
+
+    def __init__(self):
+        pass
+
+    def test(self, file_path):
+        if os.path.exists(file_path):
+            pass
+
+
+if __name__ == '__main__':
+    pass
